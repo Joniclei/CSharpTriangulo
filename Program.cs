a014774b6c8633f5bd6ccc52c51a9570cc69e837
Joniclei <jonicleibernardojunior@gmail.com>
2023-05-25 22:00:04 -0300

    b
    c

--- Program.cs
+++ Program.cs
@@ -25,11 +25,9 @@ namespace Triangulo
       y.B = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
       y.C = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
 
-      double p = ((x.A + x.B + x.C)/2.0);
-      double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+      double areaX = x.Area();
+      double areaY = y.Area();
 
-      p = ((y.A + y.B + y.C)/2.0);
-      double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
 
       Console.WriteLine($" Area de x {areaX}");
       Console.WriteLine($" Area de y {areaY}");
--- Triang.cs
+++ Triang.cs
@@ -7,5 +7,11 @@ namespace Triangulo
     public double B;
     public double C;
 
+    public double Area()
+    {
+      double p = ((A + B +C)/2.0);
+      return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+    }
+
   }
 }
